7ba53bf22dc100743482e38004aeb0dfb5bc3e53
Tanya <REINACREATES1@GMAIL.com>
2020-03-27 21:57:19 -0400

    add var

--- global.js
+++ global.js
@@ -1,50 +1,50 @@
 
 // Welcome! Here's an example of a console logged response
-console.log(4 + 5)
+console.log
 
 /***********************/
 /* Part 1: Expressions */
 /***********************/
 
-//1.) Log an expression that evaluates to the sum of two integers
+//1. Log an expression that evaluates to the sum of two integers
 // your code here 
-console.log('answer 1');
+console.log('answer 1: ', 5+5);
 
-// 2.) log an expression that evaluates to the difference of two integers
+// 2. log an expression that evaluates to the difference of two integers
 // your code here 
-console.log('answer 2');
+console.log('answer 2', 7-1);
 
-// 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
+// 3. Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here 
-console.log('answer 3');
+console.log('answer 3', "I'm writing" + "my first Javascript code");
 
-// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
+// 4. Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here 
-console.log('answer 4');
+console.log('answer 4', 7>2);
 
-// 5.) Log an expression that uses modulo, with a result of 4
+// 5. Log an expression that uses modulo, with a result of 4
 // your code here
-console.log('answer 5');
+console.log('answer 5', 8 / 2);
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
-// 6.) Assign a variable and console log the declaration
+// 6. Assign a variable and console log the declaration
 // your code here 
-console.log('answer 6');
+console.log('answer 6,' thisIsaFalse= 6>9 );
 
-// 7.) Console log a variable assigned to the sum of two numbers
-// your code here
-console.log('answer 8');
+// 7. Console log a variable assigned to the sum of two numbers
+// your code here 
+console.log('answer 8', let 16 + 1 = "17");
 
-// 8.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
+// 8. Uncomment this if else statement and add a logical expression as the condition
+//if your code here
+//    console.log(It was!)
+// else let thisIsFalse = "It was not";
 //    console.log('It was not..')
-//}
+//
 
-// 9.) Add parenthesis to the following statement, the result should be 1
-let answer = 2 * 3 + 1 / 8 + 12 / 2;
+// 9. Add parenthesis to the following statement, the result should be 1
+let answer = 3 * 3 + 1 / 8 + 12 / 2;
 console.log(answer == 1); // This should log true
