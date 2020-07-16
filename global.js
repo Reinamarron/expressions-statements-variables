94dfdbaba5d4b4e0b03eda86a77c5e12172c4346
Tanya <REINACREATES1@GMAIL.com>
2020-07-16 00:19:37 -0400

    updated answers

--- global.js
+++ global.js
@@ -8,43 +8,46 @@ console.log
 
 //1. Log an expression that evaluates to the sum of two integers
 // your code here 
-console.log('answer 1: ', 5+5);
+console.log("answer 1", 5 + 5);
 
 // 2. log an expression that evaluates to the difference of two integers
 // your code here 
-console.log('answer 2', 7-1);
+console.log("answer 2", 7 - 1);
 
 // 3. Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here 
-console.log('answer 3', "I'm writing" + "my first Javascript code");
+console.log("answer 3", "I'm writing " + "my first Javascript code");
 
 // 4. Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here 
-console.log('answer 4', 7>2);
+console.log("answer 4", 7 > 2);
 
 // 5. Log an expression that uses modulo, with a result of 4
 // your code here
-console.log('answer 5', 8 / 2);
+console.log("answer 5", 32 % 7);
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 6. Assign a variable and console log the declaration
-// your code here 
-console.log('answer 6,' thisIsaFalse= 6>9 );
+// your code here
+let answer6 = 9
+console.log(answer6);
 
 // 7. Console log a variable assigned to the sum of two numbers
 // your code here 
-console.log('answer 8', let 16 + 1 = "17");
+let answer7 = 10 + 50
+console.log("answer 7", answer7);
 
 // 8. Uncomment this if else statement and add a logical expression as the condition
-//if your code here
-//    console.log(It was!)
-// else let thisIsFalse = "It was not";
-//    console.log('It was not..')
-//
+// your code here
+if (6 > 2) { 
+  console.log("It was!")
+}  else {
+ console.log("It was not..")
+}
 
 // 9. Add parenthesis to the following statement, the result should be 1
-let answer = 3 * 3 + 1 / 8 + 12 / 2;
+let answer = (3 * 3 + 1) / (20 / 2);
 console.log(answer == 1); // This should log true
